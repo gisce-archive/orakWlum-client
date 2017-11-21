8f060eb8caba3c879080b8e62f213b9960c7d5c6
Xavi Torelló <info@xaviertorello.cat>
2017-11-21 05:46:21 +0100

    Add specs

---
+++ specs/orakwlum_api_spec.py
@@ -0,0 +1,48 @@
+import vcr
+import requests
+
+import sys
+sys.path.insert(0, '..')
+from orakwlum_api import orakWlum_API
+
+config = {
+    'url': 'http://localhost:5000/api/v1',
+    'user': "test@gisce.net",
+    'password': "test@gisce.net",
+}
+
+with description('A new'):
+    with before.each:
+        self.config = config
+
+    with context('orakWlum_API'):
+        with context('initialization'):
+            with it('must be performed as expected'):
+                with vcr.use_cassette('fixtures/okW_API_login.yaml'):
+                    self.API = orakWlum_API(**self.config)
+                    assert self.API.url == self.config['url'], "URL must match"
+                    assert self.API.token != None, "Token must be defined"
+
+            with context('errors'):
+                with it('must be handled for incorrect user'):
+                    with vcr.use_cassette('fixtures/okW_API_error_login_incorrect_user.yaml'):
+                        self.config['user'] = "non-existent-user"
+                        self.API = orakWlum_API(**self.config)
+                        assert self.API.url == self.config['url'], "URL must match"
+                        assert self.API.token == None, "Token must not be defined for erroneous login"
+
+                with it('must be handled for incorrect password'):
+                    with vcr.use_cassette('fixtures/okW_API_error_login_incorrect_passwd.yaml'):
+                        self.config['password'] = "incorrect-password"
+                        self.API = orakWlum_API(**self.config)
+                        assert self.API.url == self.config['url'], "URL must match"
+                        assert self.API.token == None, "Token must not be defined for erroneous login"
+
+        with context('consumption'):
+            with before.each:
+                self.config = config
+                self.API = orakWlum_API(**self.config)
+
+            with it('must be performed as expected for GET requests'):
+                with vcr.use_cassette('fixtures/okW_API_get.yaml'):
+                    pass
