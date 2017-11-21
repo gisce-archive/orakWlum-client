5924419cc9a04d9d73a482c71078ec83bce25419
Xavi Torelló <info@xaviertorello.cat>
2017-11-21 06:11:11 +0100

    Fix fixtures and cassettes

--- specs/orakwlum_api_spec.py
+++ specs/orakwlum_api_spec.py
@@ -1,15 +1,15 @@
 import vcr
 import requests
 
-import sys
-sys.path.insert(0, '..')
-from orakwlum_api import orakWlum_API
+from orakwlum import orakWlum_API
 
 config = {
     'url': 'http://localhost:5000/api/v1',
     'user': "test@gisce.net",
     'password': "test@gisce.net",
 }
+
+fixtures_path = 'specs/fixtures/'
 
 with description('A new'):
     with before.each:
@@ -18,21 +18,21 @@ with description('A new'):
     with context('orakWlum_API'):
         with context('initialization'):
             with it('must be performed as expected'):
-                with vcr.use_cassette('fixtures/okW_API_login.yaml'):
+                with vcr.use_cassette(fixtures_path + 'okW_API_login.yaml'):
                     self.API = orakWlum_API(**self.config)
                     assert self.API.url == self.config['url'], "URL must match"
                     assert self.API.token != None, "Token must be defined"
 
             with context('errors'):
                 with it('must be handled for incorrect user'):
-                    with vcr.use_cassette('fixtures/okW_API_error_login_incorrect_user.yaml'):
+                    with vcr.use_cassette(fixtures_path + 'okW_API_error_login_incorrect_user.yaml'):
                         self.config['user'] = "non-existent-user"
                         self.API = orakWlum_API(**self.config)
                         assert self.API.url == self.config['url'], "URL must match"
                         assert self.API.token == None, "Token must not be defined for erroneous login"
 
                 with it('must be handled for incorrect password'):
-                    with vcr.use_cassette('fixtures/okW_API_error_login_incorrect_passwd.yaml'):
+                    with vcr.use_cassette(fixtures_path + 'okW_API_error_login_incorrect_passwd.yaml'):
                         self.config['password'] = "incorrect-password"
                         self.API = orakWlum_API(**self.config)
                         assert self.API.url == self.config['url'], "URL must match"
@@ -40,9 +40,10 @@ with description('A new'):
 
         with context('consumption'):
             with before.each:
-                self.config = config
-                self.API = orakWlum_API(**self.config)
+                with vcr.use_cassette(fixtures_path + 'okW_API_login.yaml'):
+                    self.config = config
+                    self.API = orakWlum_API(**self.config)
 
             with it('must be performed as expected for GET requests'):
-                with vcr.use_cassette('fixtures/okW_API_get.yaml'):
+                with vcr.use_cassette(fixtures_path + 'okW_API_get.yaml'):
                     pass
