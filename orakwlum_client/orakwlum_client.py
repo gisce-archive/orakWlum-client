151b5889c3a97ae69de4cf61a2fc5bd8014a7318
Xavi Torelló <info@xaviertorello.cat>
2017-11-28 20:38:00 +0100

    Instanciate a Consumption at Client.consumptions
    Fix #14

--- orakwlum_client/orakwlum_client.py
+++ orakwlum_client/orakwlum_client.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .orakwlum_api import orakWlum_API
+from .consumptions import Consumptions
 
 class orakWlum_Client(object):
     def __init__(self, **config):
@@ -33,29 +34,5 @@ class orakWlum_Client(object):
         }
         self.API = orakWlum_API(**config)
 
-    def consumptions_by_cups (self, CUPS, date_start, date_end):
-        """
-        Return consumptions for a CUPS (or a list) between a range of dates
-
-        - dates must be timestamps
-        - CUPS can be a string or a list of strings with the CUPS
-        """
-        params = {
-            "date_start": date_start,
-            "date_end": date_end,
-            "cups": CUPS,
-        }
-        return self.API.get(resource="/consumptions", params=params)
-
-    def consumptions_by_aggregates (self, date_start, date_end, aggregates=None):
-        """
-        Return consumptions grouped by REE aggregates between a range of dates
-
-        - dates must be timestamps
-        - aggregates can be the list of aggregates to reach or None
-        """
-        params = {
-            "date_start": date_start,
-            "date_end": date_end,
-        }
-        return self.API.get(resource="/consumptions_by_aggregates", params=params)
+        # Prepare consumptions method
+        self.consumptions = Consumptions(self.API)
